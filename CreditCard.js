584333024f880ea3c447699ebc5abd0242e22707
Cres Jie <cresjie@gmail.com>
2016-04-01 14:37:28 +0800

    added luhn calculation

--- CreditCard.js
+++ CreditCard.js
@@ -25,7 +25,7 @@
 			'union'		 : /^(62[0-9]{14,17})$/,
 			'koreanlocal' : /^(62[0-9]{14,17})$/,
 			'bcglobal'	 : /^(6541|6556)[0-9]{12}$/,
-			'rupay'		 : /^6[0-9]{15}$/,	
+			'rupay'		 : /^(508[5-9][0-9]{12})|(6069[8-9][0-9]{11})|(607[0-8][0-9]{12})|(6079[0-8][0-9]{11})|(608[0-5][0-9]{12})|(6521[5-9][0-9]{11})|(652[2-9][0-9]{12})|(6530[0-9]{12})|(6531[0-4][0-9]{11})$/,	
 			'switch'	 : /^(4903|4905|4911|4936|6333|6759)[0-9]{12}|(4903|4905|4911|4936|6333|6759)[0-9]{14}|(4903|4905|4911|4936|6333|6759)[0-9]{15}|564182[0-9]{10}|564182[0-9]{12}|564182[0-9]{13}|633110[0-9]{10}|633110[0-9]{12}|633110[0-9]{13}$/,
 
 		};
@@ -81,6 +81,47 @@
 
 		this.getBrandNameByCode = function(code) {
 			return brandNames[code];
+		}
+
+		this.luhnCalculation = function(number){
+			var digits = [],
+				lastDigit,
+				sum = 0;
+
+			number = number.toString();
+
+			//splits number into digits
+			for(var i = 0; i < number.length; i++){
+				digits.push(number[i]);
+			}
+
+			//drops the last digits
+			lastDigit = digits.splice(-1,1)[0];
+
+			//reverse the digits
+			digits.reverse();
+
+			
+			//multiple by 2 for digits in odd position
+			for(var i = 0 ; i < digits.length; i++){
+				if( !(i%2) ){
+					digits[i] *=2 ;
+				}
+			}
+			
+			//subtract 9 for digits larger than 9
+			for(var i = 0; i < digits.length; i++ ){
+				if( parseInt(digits[i]) > 9 ){
+					digits[i] -= 9;
+				}
+			}
+
+			
+			for(var i = 0; i < digits.length; i++)
+				sum += parseInt(digits[i]);
+
+			return sum + parseInt(lastDigit);
+
 		}
 
 		if( !(this instanceof window[className]) ) {
